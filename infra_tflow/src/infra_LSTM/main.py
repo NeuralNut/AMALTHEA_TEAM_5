0163644fcfb6d153c527c41f78271a57a4f775bb
Emily Jensen <emily.jensen101@gmail.com>
2017-06-15 16:45:28 -0400

    New validation scheme
    stops training when validation loss does not decrease enough

--- infra_tflow/src/infra_LSTM/main.py
+++ infra_tflow/src/infra_LSTM/main.py
@@ -59,19 +59,19 @@ X_train,train_seq,X_val,val_seq,X_test,test_seq,y_train,y_val,y_test = load_data
 #%%
 """Define configuration of hyperparameters"""
 # Define hyperparameters also used in this file
-val_increment = 5 # how many epochs between checking validation preformance
+val_increment = 10 # how many epochs between checking validation preformance
 batch_size = 30
 
  
 
-max_epochs = 40
+max_epochs = 100
 dropout = 0.8  
 num_classes = max(y_test) + 1
 config = {'num_layers':3, # number of hidden LSTM layers
           'hidden_size':120, # number of units in each layer
           'grad_max_abs':5, # cutoff for gradient clipping
           'batch_size':batch_size,
-          'learning_rate':0.001,
+          'learning_rate':0.0008,
           'classes':num_classes,
           'dropout_keep_prob':dropout,
           'sl':X_train.shape[1],
@@ -102,11 +102,14 @@ with tf.Session() as sess:
     
 
     # Create initial values to start the loop
-    old_validation_acc = 0
-    new_validation_acc = 0.1
-    epoch = 1
+    old_validation_loss = 0.1
+    new_validation_loss = 0
+    best_val_acc = 0
+    best_epoch = 0
+    epoch = 0
     # Create training loop that ends when max epochs is reached or validation suffers
-    while epoch <= max_epochs and old_validation_acc <= new_validation_acc:
+    while epoch < max_epochs and new_validation_loss <= 0.9 * old_validation_loss:
+        epoch += 1
         # Start of new epoch, reset
         epoch_acc = 0
         # Run through each mini-batch once per epoch
@@ -123,28 +126,36 @@ with tf.Session() as sess:
             epoch_acc += model.accuracy.eval(feed_dict=train_dict) * batch_x.shape[0]
             logits = model.logits.eval(feed_dict=train_dict)
             epoch_loss = model.loss.eval(feed_dict=train_dict)
-        # Assess validation accuracy every 3 epochs
+            
+        # Check to see if this epoch is the best performing one yet
+        current_val_acc = model.accuracy.eval(feed_dict=val_dict)
+        if current_val_acc >= best_val_acc:
+            best_val_acc = current_val_acc
+            best_epoch = epoch
+        # Assess validation loss every so often
         if epoch % val_increment == 0:
-            old_validation_acc = new_validation_acc
-            new_validation_acc = model.accuracy.eval(feed_dict=val_dict)
-            validation_loss = model.loss.eval(feed_dict=val_dict)
-            print('%d | train_acc: %f | train_loss: %f | val_acc: %f | val_loss: %f' %(epoch,epoch_acc/X_train.shape[0],epoch_loss, new_validation_acc, validation_loss))
+            old_validation_loss = new_validation_loss
+            new_validation_loss = model.loss.eval(feed_dict=val_dict)
+            print('%d | train_acc: %f | train_loss: %f | val_acc: %f | val_loss: %f' %(epoch,epoch_acc/X_train.shape[0],epoch_loss, current_val_acc, new_validation_loss))
         else:
-            # not a multiple of 3, just print training data
-            print('%d | train_acc: %f | train_loss: %f' %(epoch,epoch_acc/X_train.shape[0],epoch_loss))
+            # not a multiple of the increment, just print training data
+            print('%d | train_acc: %f | train_loss: %f | val_acc: %f' %(epoch,epoch_acc/X_train.shape[0],epoch_loss,current_val_acc))
         # TODO: save weights for each epoch
-        # increment epoch counter
-        epoch += 1
     
-    # At this point, training has stopped
-    # Create train, test, and summary strings
-    train_summary_str = model.trainloss_summary.eval(feed_dict={X:X_train, y:y_train,model.seq_length:train_seq})
-    test_summary_str = model.testloss_summary.eval(feed_dict=test_dict)
-    val_summary_str = model.valloss_summary.eval(feed_dict=val_dict) 
+        # At this point, training has stopped
+        # Create train, test, and summary strings
+        train_summary_str = model.trainloss_summary.eval(feed_dict={X:X_train, y:y_train,model.seq_length:train_seq})
+        test_summary_str = model.testloss_summary.eval(feed_dict=test_dict)
+        val_summary_str = model.valloss_summary.eval(feed_dict=val_dict) 
+        
+        # Save losses and accuracies to the summary file         
+        model.file_writer.add_summary(train_summary_str, epoch)
+        model.file_writer.add_summary(test_summary_str, epoch)
+        model.file_writer.add_summary(val_summary_str, epoch)
     
     # Print the reason for stopping
-    if old_validation_acc > new_validation_acc:
-        print('Model overfitted! Stopped training after epoch %d and will use weights from epoch %d'%(epoch-1,epoch-1 - val_increment))
+    if new_validation_loss > 0.9 * old_validation_loss:
+        print('Model overfitted! Stopped training after epoch %d and will use weights from epoch %d'%(epoch,best_epoch))
         # TODO: restore last set of weights
     elif epoch > max_epochs:
         print('Reached max number of epochs')
@@ -153,13 +164,8 @@ with tf.Session() as sess:
     test_prediction = model.predictions.eval(feed_dict=test_dict)
     test_acc = model.accuracy.eval(feed_dict=test_dict)
     test_loss = model.loss.eval(feed_dict=test_dict)
-    # Save losses and accuracies to the summary file         
-    model.file_writer.add_summary(train_summary_str, epoch)
-    model.file_writer.add_summary(test_summary_str, epoch)
-    model.file_writer.add_summary(val_summary_str, epoch)
     print('Final accuracy:',test_acc,'Final cost:',test_loss)
 
-#%%    
     # create confusion matrix to visualize classification errors
     confusion_matrix_array = confusion_matrix(y_test,test_prediction)
     cf_normed = np.array(confusion_matrix_array)/np.sum(confusion_matrix_array) * 100
@@ -180,7 +186,6 @@ with tf.Session() as sess:
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
-    plt.show()
 
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
